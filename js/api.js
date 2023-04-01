72e99aa7489b42a2d160d1d3176c75a90b2f36ca
LikeValery <110396457+LikeValery@users.noreply.github.com>
2023-04-01 11:43:59 +0300

    Продолжаю работать

--- js/api.js
+++ js/api.js
@@ -1,1 +1,33 @@
-const BASE_URL =
+const BASE_URL = 'https://28.javascript.pages.academy/kekstagram';
+const Rout = {
+  GET_DATA: '/data',
+  SEND_DATA: '/',
+};
+const Method = {
+  GET: 'GET',
+  POST: 'POST',
+};
+
+const ErrorText = {
+  GET_DATA: 'Не удалось загрузитьстраницу, попробуйте еще раз',
+  SEND_DATA: 'Не удалось отправить форму, попробуйте еще раз',
+};
+
+const dataLoad = (rout, ErrorText, Method = Method.GET, body = null) => {
+  fetch(`${BASE_URL}&${rout}`, {method, body})
+    .then((response) => {
+      if(!response.ok) {
+        throw new Error();
+      }
+      return response.json();
+    })
+    .catch(() => {
+      throw new Error();
+    });
+
+const getData = () => load(Rout.GET_DATA, ErrorText.GET_DATA);
+
+const sendData = (body) =>
+    load(Rout.SEND_DATA, ErrorText.SEND_DATA, Method.POST, body);
+
+export {getData, sendData};
