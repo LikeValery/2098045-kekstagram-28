62ffe1834374ca7c25801993c2d54757f7109bd3
LikeValery <110396457+LikeValery@users.noreply.github.com>
2023-04-09 21:18:18 +0300

    еще чуток

--- js/user-photo.js
+++ js/user-photo.js
@@ -1,4 +1,10 @@
+const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 const fileChooser = document.querySelector('.upload input[type=file]');
 const preview = document.querySelector('.img-upload__preview img');
 
+fileChooser.addEventListener('change', () => {
+  const file = fileChooser.files[0];
+  const fileName = file.name.toLowerCase();
 
+  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+});
